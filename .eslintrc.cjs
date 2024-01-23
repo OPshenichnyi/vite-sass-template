3f5c91d3f4e503762dde92edea28398fb8410fa9
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-01-23 13:24:54 +0200

    add settings prettier and esLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,24 +3,26 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  plugins: "prettier",
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
+    'no-var': 'error',
+    'no-unused-vars': 'warn',
+    'prefer-const': 'off',
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }],
   },
 };
