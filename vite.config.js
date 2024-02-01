a07dbec6dc448a1ce1eae240281dec29e2f5a4ed
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-02-01 15:39:43 +0200

    add svg-sprite createor and add format html

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import imagemin from "imagemin";
-import imageminWebp from "imagemin-webp";
-import path from "path";
-import { defineConfig } from "vite";
-import glob from "fast-glob";
-import { fileURLToPath } from "url";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import imagemin from 'imagemin';
+import imageminWebp from 'imagemin-webp';
+import path from 'path';
+import { defineConfig } from 'vite';
+import glob from 'fast-glob';
+import { fileURLToPath } from 'url';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import IconSpritePlugin from './plugins/vite-plugin-icon-sprite';
 
 export default defineConfig({
   plugins: [
+    IconSpritePlugin(),
     ViteImageOptimizer({
       png: {
         quality: 86,
@@ -20,11 +22,11 @@ export default defineConfig({
       },
     }),
     {
-      ...imagemin(["./src/img/**/*.{jpg,png,jpeg}"], {
-        destination: "./src/img/webp/",
+      ...imagemin(['./src/img/**/*.{jpg,png,jpeg}'], {
+        destination: './src/img/webp/',
         plugins: [imageminWebp({ quality: 86 })],
       }),
-      apply: "serve",
+      apply: 'serve',
     },
   ],
   build: {
@@ -32,8 +34,8 @@ export default defineConfig({
     rollupOptions: {
       input: Object.fromEntries(
         glob
-          .sync(["./*.html", "./pages/**/*.html"])
-          .map((file) => [
+          .sync(['./*.html', './pages/**/*.html'])
+          .map(file => [
             path.relative(
               __dirname,
               file.slice(0, file.length - path.extname(file).length)
@@ -43,7 +45,7 @@ export default defineConfig({
       ),
       // output unminified CSS file
       output: {
-        assetFileNames: "assets/[name].[ext]",
+        assetFileNames: 'assets/[name].[ext]',
       },
     },
   },
